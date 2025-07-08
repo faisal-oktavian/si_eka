2395d282542c6f673683375daf588ceedae83cdf
faisal-oktavian <oktavianfaisal97@gmail.com>
2025-07-08 18:04:07 +0700

    tambah menu master kendaraan

---
+++ application/views/kendaraan/vf_kendaraan.php
@@ -0,0 +1,18 @@
+<form class="form-horizontal">
+	<div class="form-group">
+		<label class="control-label col-sm-2">Nama Kendaraan</label>
+		<div class="col-md-4 col-sm-6">
+			<input type="text" class="form-control" name="vf_nama_kendaraan" id="vf_nama_kendaraan" placeholder="Nama Kendaraan">
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-sm-2">Status</label>
+		<div class="col-sm-4">
+			<select class="form-control" name="vf_is_active" id="vf_is_active">
+				<option value="">Semua</option>
+				<option value="1">Aktif</option>
+				<option value="0">Non Aktif</option>
+			</select>
+		</div>
+	</div>
+</form>
