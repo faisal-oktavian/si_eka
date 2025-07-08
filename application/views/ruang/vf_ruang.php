370dd234eab2712bc1e4eeb4b0dfc5a80fde5957
faisal-oktavian <oktavianfaisal97@gmail.com>
2025-07-08 17:50:14 +0700

    tambah menu master ruang

---
+++ application/views/ruang/vf_ruang.php
@@ -0,0 +1,18 @@
+<form class="form-horizontal">
+	<div class="form-group">
+		<label class="control-label col-sm-2">Nama Ruang</label>
+		<div class="col-md-4 col-sm-6">
+			<input type="text" class="form-control" name="vf_nama_ruang" id="vf_nama_ruang" placeholder="Nama Ruang">
+		</div>
+	</div>
+	<div class="form-group">
+		<label class="control-label col-sm-2">Status</label>
+		<div class="col-sm-4">
+			<select class="form-control" name="vf_is_active" id="vf_is_active">
+				<option value="">Semua</option>
+				<option value="1">Aktif</option>
+				<option value="0">Non Aktif</option>
+			</select>
+		</div>
+	</div>
+</form>
